d152539528657a4c5506c4b4b1de981ea074e460
Jeff <jeffrey.disbrow@gmail.com>
2019-03-04 14:28:32 -0600

    updated not ordered

--- fp_archive_analysis/not_ordered.py
+++ fp_archive_analysis/not_ordered.py
@@ -7,6 +7,63 @@ Created on Mon Mar  4 11:40:27 2019
 
 import geopandas as gpd
 import pandas as pd
-from query_danco import stereo_noh
+from query_danco import stereo_noh, query_footprint
 
-intrack_noh = stereo_noh()
+# Load data
+all_oh = query_footprint('dg_imagery_index_all_onhand') # All ids onhand
+
+intrack_noh = stereo_noh() # Intrack cc20 not on hand
+intrack_noh = intrack_noh[['acqdate', 'catalogid']] # Drop all other columns
+
+# All crosstrack
+xtrack_gdb_path = r'C:\Users\disbr007\imagery\dg_cross_track_2019jan09_deliv.gdb'
+xtrack = gpd.read_file(
+        xtrack_gdb_path, 
+        crs = {'init': 'espg:4326'}, 
+        driver='OpenFileGDB', 
+        layer='dg_imagery_index_all_cc20_xtrack_pairs_jan09_prj_WGS')
+
+## Cross track both ids
+# Get unique ids from both catalogid1 and catalogid2 columns
+xtrack1_ids = set(list(xtrack.catalogid1.values))
+xtrack2_ids = set(list(xtrack.catalogid2.values))
+xtrack2_unq_ids = [x for x in xtrack2_ids if x not in xtrack1_ids]
+
+# Create dataframes of ids in catid1 and catid2
+xtrack1 = xtrack[['catalogid1', 'acqdate1', 'sqkm']][xtrack.catalogid1.isin(xtrack1_ids)]
+xtrack2 = xtrack[['catalogid2', 'acqdate2', 'sqkm']][xtrack.catalogid2.isin(xtrack2_unq_ids)]
+
+# Drop duplicate rows
+xtrack1.drop_duplicates('catalogid1', keep='first', inplace=True)
+xtrack2.drop_duplicates('catalogid2', keep='first', inplace=True)
+
+# Rename for concat
+xtrack1.rename(index=str, columns={'catalogid1': 'catalogid', 'acqdate1': 'acqdate'}, inplace=True)
+xtrack2.rename(index=str, columns={'catalogid2': 'catalogid', 'acqdate2': 'acqdate'}, inplace=True)
+
+# Add catid1 and catid2 dataframes to get unique xtrack ids and acqdates
+xtrack_unq = pd.concat([xtrack1, xtrack2])
+
+# Cross track not onhand
+ids_oh = list(all_oh.catalogid) # list of all IDs onhand
+xtrack_noh = xtrack_unq[~xtrack_unq.catalogid.isin(ids_oh)] # all xtrack ids not onhand
+
+# Monthly grouping
+intrack_noh['acqdate'] = pd.to_datetime(intrack_noh['acqdate'])
+xtrack_noh['acqdate'] = pd.to_datetime(xtrack_noh['acqdate'])
+
+intrack_noh.set_index('acqdate', inplace=True)
+xtrack_noh.set_index('acqdate', inplace=True)
+
+# Break out cross track area bins
+xtrack_aggregation = {'catalogid': 'nunique', 'sqkm': 'sum'}
+monthly_xtrack_noh = xtrack_noh.groupby(pd.Grouper(freq='M')).agg(xtrack_aggregation)
+monthly_xtrack_1k = xtrack_noh[xtrack_noh['sqkm'] >= 1000.0].groupby(pd.Grouper(freq='M')).agg(xtrack_aggregation)
+monthly_xtrack_500 = xtrack_noh[(xtrack_noh['sqkm'] >= 500.0) & (xtrack_noh['sqkm'] < 1000.0)].groupby(pd.Grouper(freq='M')).agg(xtrack_aggregation)
+monthly_xtrack_250 = xtrack_noh[(xtrack_noh['sqkm'] >= 250.0) & (xtrack_noh['sqkm'] < 500.0)].groupby(pd.Grouper(freq='M')).agg(xtrack_aggregation)
+monthly_xtrack_0 = xtrack_noh[xtrack_noh['sqkm'] < 250.0].groupby(pd.Grouper(freq='M')).agg(xtrack_aggregation)
+
+monthly_intrack_noh = intrack_noh.groupby(pd.Grouper(freq='M')).agg({'catalogid': 'nunique'})
+
+# Output to excel
+
