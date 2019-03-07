69a8ce8c6a86559e87ec8da4d9257dd3a21a64b8
Jeff <jeffrey.disbrow@gmail.com>
2019-03-07 12:05:12 -0600

    updated select_stereo_danco.py

--- fp_selection_utils/select_stereo_danco.py
+++ fp_selection_utils/select_stereo_danco.py
@@ -11,9 +11,10 @@ import os
 
 from query_danco import query_footprint
 
-stereo_all = query_footprint('dg_imagery_index_stereo') # all dg stereo as df
-dg_index_cc20 = query_footprint('dg_imagery_index_all_cc20') # all dg imagery cc20 as df
-dg_stereo_catalogids = query_footprint('dg_stereo_catalogids', table=True)
+#stereo_all = query_footprint('dg_imagery_index_stereo') # all dg stereo as df
+#dg_index_cc20 = query_footprint('dg_imagery_index_all_cc20') # all dg imagery cc20 as df
+#dg_stereo_catalogids = query_footprint('dg_stereo_catalogids', table=True)
+dg_all = query_footprint('index_dg')
 
 # text file containing ids, one/line to select from dg footprint
 ids_path = r'C:\Users\disbr007\imagery_orders\not_onhand\stereo_ids_not_onhand.txt'
@@ -24,6 +25,7 @@ with open(ids_path, 'r') as f:
         ids.append(line.strip())
 
 # Select rows from df's that are in ids list
-stereo_noh_corrected = stereo_all[stereo_all['catalogid'].isin(ids)] #only returning 12,223
-stereo_noh2 = dg_index_cc20[dg_index_cc20['catalogid'].isin(ids)]
-stereo_noh_tbl = dg_stereo_catalogids[dg_stereo_catalogids['catalogid'].isin(ids)]
+#stereo_noh_corrected = stereo_all[stereo_all['catalogid'].isin(ids)] #only returning 12,223
+#stereo_noh2 = dg_index_cc20[dg_index_cc20['catalogid'].isin(ids)]
+#stereo_noh_tbl = dg_stereo_catalogids[dg_stereo_catalogids['catalogid'].isin(ids)]
+stereo_noh_from_all = dg_all[dg_all['catalogid'].isin(ids)]
