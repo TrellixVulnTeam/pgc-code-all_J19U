8ee954dbf322bffb9411e163b69802b70002caf2
Jeff <jeffrey.disbrow@gmail.com>
2019-03-06 21:50:11 -0600

    working stereo notonhand code

--- img_orders/update_notonhand.py
+++ img_orders/update_notonhand.py
@@ -9,6 +9,8 @@ import geopandas as gpd
 import pandas as pd
 import os
 
+from query_danco import stereo_noh
+
 # IMA Ordered IDs
 all_paths = [
         r"C:\Users\disbr007\imagery_orders\not_onhand\all_order_2019march06_1.csv",
@@ -24,18 +26,44 @@ for tbl in all_paths:
             all_ordered.append(x)
             
 
-# All of Thomas' DG folders to pull most recent ids from, need to determine where cutoff in index is
-
-dg_ftp_folder = r'Y:\private\imagery\satellite\_footprints\AGIC_footprint\DG_ftp_v5'
-dg_ftp_shps = {}
-# loop through all shapefiles, store filenames by date, which is suffix of filename before .shp
-for filename in os.listdir(dg_ftp_folder):
-    if filename.endswith('.shp'):
-        ftp_num = filename.split('_')[1]
-        ftp_num = int(ftp_num.strip('ftp'))
-        dg_ftp_shps[ftp_num] = filename
+## All of Thomas' DG folders to pull most recent ids from, need to determine where cutoff in index is
+#dg_ftp_folder = r'Y:\private\imagery\satellite\_footprints\AGIC_footprint\DG_ftp_v5'
+#dg_ftp_shps = {}
+## loop through all shapefiles, store filenames by date, which is suffix of filename before .shp
+#for filename in os.listdir(dg_ftp_folder):
+#    if filename.endswith('.shp'):
+#        ftp_num = filename.split('_')[1]
+#        ftp_num = ftp_num.strip('ftp')
+#        dg_ftp_shps[ftp_num] = filename
 
 # Read index into geopandas        
-index_path = r"E:\disbr007\UserServicesRequests\pgcImageryIndexV6_2019jan12.gdb"
-index = gpd.read_file(index_path, driver='OpenFileGDB', layer='pgcImageryIndexV6_2019feb04')
-last_o_drive = max(list(index['O_DRIVE']))
+
+#index_path = r"C:\Users\disbr007\pgc_index\pgcImageryIndexV6_2019feb04.gdb"
+#index = gpd.read_file(index_path, driver='OpenFileGDB', layer='pgcImageryIndexV6_2019feb04')
+#print('Index loaded into geopandas...')
+
+index_path = r"C:\Users\disbr007\imagery_orders\not_onhand\index.txt"
+index = pd.read_csv(index_path)
+print("index loaded")
+
+# For searching via Thomas' shapefiles...
+#index_o_drives = list(index['O_DRIVE'])
+#index_o_drives = [x.strip('DG_ftp') for x in index_o_drives if x.startswith('DG_ftp')]
+
+index_ids = list(set(list(index['CATALOG_ID'])))
+
+nasa_path = r"C:\Users\disbr007\imagery_orders\not_onhand\nga_inventory20190219.txt"
+nasa = pd.read_csv(nasa_path)
+nasa_ids = list(set(list(nasa['CATALOG_ID'])))
+print('nasa loaded')
+
+all_oh = all_ordered + index_ids + nasa_ids
+all_oh = list(set(all_oh))
+
+stereo_noh = stereo_noh()
+
+s_noh_ids = list(stereo_noh['catalogid'])
+
+s_noh_remove = [x for x in s_noh_ids if x not in all_oh]
+list(set(s_noh_ids).difference(all_oh))
+
