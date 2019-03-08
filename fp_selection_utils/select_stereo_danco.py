08cc1f10341c8f46102d5da6f38a596cc4291620
Jeff <jeffrey.disbrow@gmail.com>
2019-03-08 11:12:19 -0600

    updates

--- fp_selection_utils/select_stereo_danco.py
+++ fp_selection_utils/select_stereo_danco.py
@@ -14,7 +14,7 @@ from query_danco import query_footprint
 #stereo_all = query_footprint('dg_imagery_index_stereo') # all dg stereo as df
 #dg_index_cc20 = query_footprint('dg_imagery_index_all_cc20') # all dg imagery cc20 as df
 #dg_stereo_catalogids = query_footprint('dg_stereo_catalogids', table=True)
-dg_all = query_footprint('index_dg')
+dg_all = query_footprint('index_dg') # entire dg index
 
 # text file containing ids, one/line to select from dg footprint
 ids_path = r'C:\Users\disbr007\imagery_orders\not_onhand\stereo_ids_not_onhand.txt'
@@ -29,3 +29,10 @@ with open(ids_path, 'r') as f:
 #stereo_noh2 = dg_index_cc20[dg_index_cc20['catalogid'].isin(ids)]
 #stereo_noh_tbl = dg_stereo_catalogids[dg_stereo_catalogids['catalogid'].isin(ids)]
 stereo_noh_from_all = dg_all[dg_all['catalogid'].isin(ids)]
+stereo_noh_from_all_cc20 = stereo_noh_from_all[stereo_noh_from_all.cloudcover <= 20]
+stereo_noh_from_all_cc20 = stereo_noh_from_all_cc20[stereo_noh_from_all_cc20.acqdate < '2019-01-01']
+
+excel_path = r'E:\disbr007\imagery_orders\PGC_order_2019march08_intrack_not_onhand_thru_EOY2018\intrack_not_onhand_thru_EOY2018_master.xlsx'
+excel_writer = pd.ExcelWriter(excel_path)
+stereo_noh_from_all_cc20.to_excel(excel_writer, index=True)
+excel_writer.save()
--- img_orders/update_notonhand.py
+++ img_orders/update_notonhand.py
@@ -25,55 +25,46 @@ for tbl in all_paths:
         for x in content:
             all_ordered.append(x)
             
-
-## All of Thomas' DG folders to pull most recent ids from, need to determine where cutoff in index is
-#dg_ftp_folder = r'Y:\private\imagery\satellite\_footprints\AGIC_footprint\DG_ftp_v5'
-#dg_ftp_shps = {}
-## loop through all shapefiles, store filenames by date, which is suffix of filename before .shp
-#for filename in os.listdir(dg_ftp_folder):
-#    if filename.endswith('.shp'):
-#        ftp_num = filename.split('_')[1]
-#        ftp_num = ftp_num.strip('ftp')
-#        dg_ftp_shps[ftp_num] = filename
-
-# Read index into geopandas        
-
+## Read index into geopandas
 #index_path = r"C:\Users\disbr007\pgc_index\pgcImageryIndexV6_2019feb04.gdb"
 #index = gpd.read_file(index_path, driver='OpenFileGDB', layer='pgcImageryIndexV6_2019feb04')
-#print('Index loaded into geopandas...')
-
+# Read text version of master footprint into pandas
 index_path = r"C:\Users\disbr007\imagery_orders\not_onhand\index.txt"
 index = pd.read_csv(index_path)
-print("index loaded")
-
-# For searching via Thomas' shapefiles...
-#index_o_drives = list(index['O_DRIVE'])
-#index_o_drives = [x.strip('DG_ftp') for x in index_o_drives if x.startswith('DG_ftp')]
-
+print('index loaded')
+# List of unique ids in the index
 index_ids = list(set(list(index['CATALOG_ID'])))
 
+# Read text version of nasa database into pandas
 nasa_path = r"C:\Users\disbr007\imagery_orders\not_onhand\nga_inventory20190219.txt"
 nasa = pd.read_csv(nasa_path)
+# List unique ids in nasa db
 nasa_ids = list(set(list(nasa['CATALOG_ID'])))
 print('nasa loaded')
 
+# Combine all ordered, pgc index and nasa index ids
 all_oh = all_ordered + index_ids + nasa_ids
+# Get unique ids of combined list
 all_oh = list(set(all_oh))
+
+# Write list of unique ids "onhand" to file
 all_oh_out = r'C:\Users\disbr007\imagery_orders\not_onhand\all_ids_onhand.txt'
 with open(all_oh_out, 'w') as all_out:
-    for x in all_out:
+    for x in all_oh:
         all_out.write('{}\n'.format(x))
 
+# Get stereo not onhand from 'stereo_not_onhand_left' and 'stereo_not_onhand_right'
 stereo_noh = stereo_noh()
+# List ids in stereo not_on_hand
+stereo_noh_ids = list(stereo_noh['catalogid'])
 
-s_noh_ids = list(stereo_noh['catalogid'])
+# Remove all on hand ids from list of not on hand -> this theoretically shouldn't be necessary but is for some reason - talk to CLaire
+stereo_noh_clean = list(set(stereo_noh_ids).difference(all_oh))
 
-s_noh_remove = list(set(s_noh_ids).difference(all_oh))
-
-
+# Write stereo ids not on hand to list
 txt_out_path = r'C:\Users\disbr007\imagery_orders\not_onhand\stereo_ids_not_onhand.txt'
 with open(txt_out_path, 'w') as f:
-    for x in s_noh_remove:
+    for x in stereo_noh_clean:
         f.write('{}\n'.format(x))
 
 
