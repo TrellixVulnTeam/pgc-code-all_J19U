5439f407b3c67405a9114a7e3680d25b1ab559d5
Jeff <jeffrey.disbrow@gmail.com>
2020-01-03 14:05:34 -0600

    added arparge/CLI to multi_clip2shp.py

--- one_off/multi_clip2shp.py
+++ one_off/multi_clip2shp.py
@@ -7,8 +7,8 @@ Clips imagery in subdirectories to corresponding AOIs. AOIs shapefile
 must have a field that matches the subdirectory names.
 """
 
+import argparse
 import os
-import logging
 
 import geopandas as gpd
 
@@ -16,60 +16,88 @@ from clip2shp_bounds import warp_rasters
 from logging_utils import create_logger
 
 
-# logger = logging.getLogger('multi_clip2shp')
-# logger.setLevel(logging.DEBUG)
-# # create console handler with a higher log level
-# ch = logging.StreamHandler()
-# ch.setLevel(logging.INFO)
-# # create formatter and add it to the handlers
-# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# ch.setFormatter(formatter)
-# # add the handlers to the logger
-# logger.addHandler(ch)
-
-
-
-# Inputs
-PRJ_DIR = r'V:\pgc\data\scratch\jeff\deliverables\4056_jclark'
-RAW_DIR = os.path.join(PRJ_DIR, 'raw')
-CLIP_DIR = os.path.join(PRJ_DIR, 'clipped')
-AOIS = os.path.join(PRJ_DIR, 'BITE_buffers.shp')
-
-
 # Create logger
 logger = create_logger('multi_clip2shp',
                        handler_type='sh',
                        handler_level='DEBUG')
 
-# Load AOI polygons
-aoi_master = gpd.read_file(AOIS)
 
-
-# List imagery subdirectories
-subdirs = os.listdir(RAW_DIR)
-
-
-# Loop over imagery subdirectories and clip to appropriate AOI
-for sd in subdirs:
-    logger.info('Working on subdirectory: {}'.format(sd))
-    # Select only aoi matching the current subdirectory and write (deleted later)
-    aoi = aoi_master[aoi_master['subfolder']==int(sd)]
-    aoi_outpath = os.path.join(PRJ_DIR, 'prj_files', 'temp', 'aoi_{}.shp'.format(sd))
-    if not os.path.exists(os.path.dirname(aoi_outpath)):
-        os.makedirs(os.path.dirname(aoi_outpath))
-    aoi.to_file(aoi_outpath)
+def multi_clip2shp(aoi_master, raster_parent_dir, clipped_dir=None, return_rasters=False):
+    """
+    Take an aoi master OGR file with polygons for multiple AOIs and clips rasters in
+    subdirectories in raster_parent_dir to the aoi corresponding to the subdirectory name.
     
-    # Create out subdirectory for clipped imagery
-    out_subdir = os.path.join(CLIP_DIR, '{}_clip'.format(sd))
-    if not os.path.exists(out_subdir):
-        os.makedirs(out_subdir)
-    # Loop over each image in subdirectory and clip to AOI
-    for root, dirs, files in os.walk(os.path.join(RAW_DIR, sd)):
-        rasters = [os.path.join(root, f)
-                   for f in files 
-                   if f.endswith('.tif') or f.endswith('.ntf')]
-    
-    logger.debug('Number of rasters: {}'.format(len(rasters)))
-    logger.debug('Using {} as clip boundary...'.format(os.path.basename(aoi_outpath)))
-    # warp_rasters(aoi_outpath, rasters=rasters, out_dir=out_subdir)
+    Parameters
+    -------
+    aoi_master : STR
+        Path to AOI data source (any OGR supported format)
+    raster_parent_dir : STR
+        Path to parent directory holding subdirectories of rasters
+    clipped_dir : STR
+        Path to output clipped rasters (in subdirectories), existing or new
+    return_rasters : BOOLEAN
+        Set to True to return dictionary of clipped raster path and GDAL datasource
+        (WARNING: MEMORY INTENSIVE)
         
+    Returns
+    -------
+    LIST
+        List of paths to newly clipped rasters
+    """
+    # Create clipped directory if it doesn't exists
+    if clipped_dir is None:
+        clipped_dir = os.path.join(os.path.dirname(raster_parent_dir), 'clipped')
+        if not os.path.exists(clipped_dir):
+            os.makedirs(clipped_dir)
+    
+    # Load AOI polygons
+    aoi_master = gpd.read_file(aoi_master)
+    
+    
+    # List imagery subdirectories
+    subdirs = os.listdir(raster_parent_dir)
+    
+    
+    # Loop over imagery subdirectories and clip to appropriate AOI
+    if return_rasters is True:
+        # Create master dictionary of clipped raster paths and GDAL objects
+        clipped = {}
+    for sd in subdirs:
+        logger.info('Working on subdirectory: {}'.format(sd))
+        # Select only aoi matching the current subdirectory and write (deleted later)
+        aoi = aoi_master[aoi_master['subfolder']==int(sd)]
+        aoi_outpath = os.path.join(os.path.dirname(raster_parent_dir), 'prj_files', 'temp', 'aoi_{}.shp'.format(sd))
+        if not os.path.exists(os.path.dirname(aoi_outpath)):
+            os.makedirs(os.path.dirname(aoi_outpath))
+        aoi.to_file(aoi_outpath)
+        
+        # Create out subdirectory for clipped imagery
+        out_subdir = os.path.join(clipped_dir, '{}_clip'.format(sd))
+        if not os.path.exists(out_subdir):
+            os.makedirs(out_subdir)
+        # Loop over each image in subdirectory and clip to AOI
+        for root, dirs, files in os.walk(os.path.join(raster_parent_dir, sd)):
+            rasters = [os.path.join(root, f)
+                       for f in files 
+                       if f.endswith('.tif') or f.endswith('.ntf')]
+        
+        logger.debug('Number of rasters: {}'.format(len(rasters)))
+        logger.debug('Using {} as clip boundary...'.format(os.path.basename(aoi_outpath)))
+        
+        # Perform clipping for current subdirectory
+        clipped_subdir_rasters = warp_rasters(aoi_outpath, rasters=rasters, out_dir=out_subdir)
+        
+        # Add dictionary of clipped paths and GDAL datasources to master dict
+        if return_rasters is True:
+            clipped.extend(clipped_subdir_rasters)
+            
+    return clipped
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('raster_directory', type=os.path.abspath,
+                        help='Path to directory holding subdirectories of rasters')
+    parser.add_argument('aoi_path', type=os.path.abspath,
+                        help='Path to AOI file with polygons to clip to.')
