39afaf7ccea588f1e0b32644c2bdc82ae5f555ad
Jeff <jeffrey.disbrow@gmail.com>
2020-01-08 15:18:23 -0600

    updates

--- misc_utils/tif_color_ramp.py
+++ misc_utils/tif_color_ramp.py
@@ -12,24 +12,20 @@ import subprocess
 
 
 # Inputs
-parent_dir = r'V:\pgc\data\scratch\claire\pgc\arcticdem\mosaic\2m_v4\38_01'
+parent_dir = r'E:\disbr007\temp\arctic_dem_cmap'
 color_map = r'cmap.txt'
 
+
+stack_count_tifs = []
 for root, dirs, files in os.walk(parent_dir):
-    stack_count_tifs = []
     for f in files:
-        if f.endswith('_N.tif'):
+        if f.endswith('10m_N.tif'):
             stack_count_tifs.append(os.path.join(root, f))
             
 for tif in stack_count_tifs:
-    # tif_dir = os.path.dirname(tif)
-    # tif_name = os.path.basename(tif).split('.')[0]
-    # png_out = '{}_cmap.png'.format(tif_name)
-    # cmd = ['gdaldem', 'color-relief', tif, png_out, '-nearest_color_entry', '-of PNG', 'alpha']
-    # print(cmd)
-    print(tif)
-
-
-
-
-
+    tif_dir = os.path.dirname(tif)
+    tif_name = os.path.basename(tif).split('.')[0]
+    png_out = '{}_cmap.png'.format(tif_name)
+    cmd = ['gdaldem', 'color-relief', tif, png_out, 
+           '-nearest_color_entry', '-of PNG', '-alpha']
+    print(cmd)
