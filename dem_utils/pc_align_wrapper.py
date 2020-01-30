0a470a2eb6b93d14996b8154a31648b35ec47f8e
Jeff <jeffrey.disbrow@gmail.com>
2020-01-29 21:08:57 -0600

    fleshed out pc_align_wrapper.py

--- dem_utils/pc_align_wrapper.py
+++ dem_utils/pc_align_wrapper.py
@@ -5,9 +5,12 @@ Created on Tue Jan 28 21:53:45 2020
 @author: disbr007
 """
 
+import argparse
 import os
 import subprocess
 from subprocess import PIPE
+
+from misc_utils.logging_utils import create_logger
 
 
 # INPUTS
@@ -17,31 +20,75 @@ dem3 = r''
 
 align_dir = r''
 
-# PARAMETERS
-dem1_name = os.path.basename(dem1).split('.')
-dem2_name = os.path.basename(dem2).split('.')
+
+def main(dem1, dem2, out_dir):
+    """
+    Aligns DEMs and writes outputs to out_dir.
+
+    Parameters
+    ----------
+    dem1 : os.path.abspath
+        Path to the reference DEM.
+    dem2 : os.path.abspath
+        Path to the DEM to translate.
+    out_dir : os.path.abspath
+        Path to write alignment files to.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    logger = create_logger(os.path.basename(__file__), 'sh')
+    
+    # PARAMETERS
+    dem1_name = os.path.basename(dem1).split('.')
+    dem2_name = os.path.basename(dem2).split('.')
+    
+    
+    #### FUNCTION DEFINITION ####
+    def run_subprocess(command):
+        proc = subprocess.Popen(command, stdout=PIPE, stderr=PIPE, shell=True)
+        output, error = proc.communicate()
+        print('Output: {}'.format(output))
+        print('Err: {}'.format(error))
+    
+    
+    
+    #### RUN PC_ALIGN ####
+    max_displacement = 10
+    threads = 16
+    prefix = 'pca_{}_{}'.format(dem1_name, dem2_name)
+    
+    pca_command = """pc_align --save-transformed-source-points 
+                    --max-displacement {} 
+                    --threads {} 
+                    {} {} 
+                    -o {}""".format(max_displacement,
+                                    threads,
+                                    dem1,
+                                    dem2,
+                                    os.path.join(align_dir, prefix))
+    logger.info('pc_align command:\n'.format(pca_command))
+    logger.info(pca_command)
 
 
-#### FUNCTION DEFINITION ####
-def run_subprocess(command):
-    proc = subprocess.Popen(command, stdout=PIPE, stderr=PIPE, shell=True)
-    output, error = proc.communicate()
-    print('Output: {}'.format(output))
-    print('Err: {}'.format(error))
-
-
-
-#### RUN PC_ALIGN ####
-max_displacement = 10
-threads = 16
-prefix = 'pca_{}_{}'.format(dem1_name, dem2_name)
-
-pca_command = """pc_align --save-transformed-source-points 
-                --max-displacement {} 
-                --threads {} 
-                {} {} 
-                -o {}""".format(max_displacement,
-                                threads,
-                                dem1,
-                                dem2,
-                                os.path.join(align_dir, prefix))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('dem1', type=os.path.abspath,
+                        help='Path to the DEM to align to, the reference DEM.')
+    parser.add_argument('dem2', type=os.path.abspath,
+                        help='Path to the DEM to translate.')
+    parser.add_argument('out_dir', type=os.path.abspath,
+                        help='Path to write output files to.')
+    
+    args = parser.parse_args()
+    
+    dem1 = args.dem1
+    dem2 = args.dem2
+    out_dir = args.out_dir
+    
+    main(dem1, dem2, out_dir)
+    
