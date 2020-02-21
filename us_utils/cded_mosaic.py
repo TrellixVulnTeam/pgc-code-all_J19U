2bc63fd7ee7a8716a0486dd6f14c6a1c504afc9f
Jeff <jeffrey.disbrow@gmail.com>
2020-02-21 08:34:24 -0600

    made cded_mosaic.py Linux/terranova compatible

--- us_utils/cded_mosaic.py
+++ us_utils/cded_mosaic.py
@@ -9,6 +9,7 @@ CDED Mosaicker based on AOI
 import argparse
 import logging.config
 import os
+import platform
 import subprocess
 import zipfile
 
@@ -34,14 +35,30 @@ def run_subprocess(command):
 
 def main(args):
 
+    # Parse arguments
     aoi_path = args.aoi
     resolution = args.resolution
     out_mosaic = args.out_mosaic
     local_tiles_path = args.local_tiles_path
 
+    # Determine operating system
+    system = platform.system()
+    if system == 'Windows':
+        params = {'def_local_tiles_path': r'E:\disbr007\general\elevation\cded\50k_mosaics\CDED_tiles',
+                  'cded_50_idx': r'V:\pgc\data\elev\dem\cded\index\decoupage_snrc50k_2.shp',
+                  'cded_250_idx': r'V:\pgc\data\elev\dem\cded\index\decoupage_snrc250k_2.shp',
+                  'cded_50_tiles': r'V:\pgc\data\elev\dem\cded\50k_dem',
+                  'cded_250_tiles': r'V:\pgc\data\elev\dem\cded\250k_dem'}
+    elif system = 'Linux':
+        params = {'def_local_tiles_path': r'/mnt/pgc/data/scratch/jeff/elev/cded/tiles',
+                  'cded_50_idx': r'/mnt/pgc/data/elev/dem/cded/index/decoupage_snrc50k_2.shp',
+                  'cded_250_idx': r'/mnt/pgc/data/elev/dem/cded/index/decoupage_snrc50k_2.shp',
+                  'cded_50_tiles': r'/mnt/pgc/data/elev/dem/cded/50k_dem/',
+                  'cded_250_tiles': r'/mnt/pgc/data/elev/dem/cded/250k_dem/'}
 
     if not local_tiles_path:
-        local_tiles_path = r'E:\disbr007\general\elevation\cded\50k_mosaics\CDED_tiles'
+        # local_tiles_path = r'E:\disbr007\general\elevation\cded\50k_mosaics\CDED_tiles'
+        local_tiles_path = params['def_local_tiles_path']
         logger.debug('No local tiles path specified, using default:\n{}'.format(local_tiles_path))
     if not os.path.exists(local_tiles_path):
         os.makedirs(local_tiles_path)
@@ -49,15 +66,22 @@ def main(args):
 
     ## Choose 50k or 250k
     logger.debug('Using selected resolution: {}'.format(resolution))
-    cded_50k_index_path = r'V:\pgc\data\elev\dem\cded\index\decoupage_snrc50k_2.shp'
-    cded_250k_index_path = r'V:\pgc\data\elev\dem\cded\index\decoupage_snrc250k_2.shp'
+    # cded_50k_index_path = params['cded_50_idx']
+    # cded_250k_index_path = params['cded_250_idx']
+    # cded_50k_index_path = r'V:\pgc\data\elev\dem\cded\index\decoupage_snrc50k_2.shp'
+    # cded_250k_index_path = r'V:\pgc\data\elev\dem\cded\index\decoupage_snrc250k_2.shp'
     
     if resolution == 50:
-        index_path = cded_50k_index_path
-        tiles_path = r'V:\pgc\data\elev\dem\cded\50k_dem'
+        # index_path = cded_50k_index_path
+        # tiles_path = r'V:\pgc\data\elev\dem\cded\50k_dem'
+        index_path = params['cded_50_idx']
+        tiles_path = params['cded_50_tiles']
     elif resolution == 250:
-        index_path = cded_250k_index_path
-        tiles_path = r'V:\pgc\data\elev\dem\cded\250k_dem'
+        # index_path = cded_250k_index_path
+        # tiles_path = r'V:\pgc\data\elev\dem\cded\250k_dem')
+        index_path = params['cded_250_idx']
+        tiles_path = params['cded_250_tiles']
+
     else:
         logger.warning('Index footprint not found')
     logger.debug('Using tiles located at: {}'.format(tiles_path))
