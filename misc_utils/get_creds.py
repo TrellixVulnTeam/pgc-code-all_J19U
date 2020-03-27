e2279303a9674ec10911c83c2804de763c5fe809
Jeff <jeffrey.disbrow@gmail.com>
2020-03-27 14:55:12 -0500

    trying to get creds to work in colab

--- misc_utils/get_creds.py
+++ misc_utils/get_creds.py
@@ -14,7 +14,8 @@ def get_creds(creds_loc=r"C:\code\pgc-code-all\config\cred.txt"):
     
     system = platform.system()
     if system == 'Linux':
-        creds_loc = os.path.join('/mnt', 'pgc', 'data', 'scratch', 'jeff', 'code', 'pgc-code-all', 'config', 'cred.txt')
+        # creds_loc = os.path.join('/mnt', 'pgc', 'data', 'scratch', 'jeff', 'code', 'pgc-code-all', 'config', 'cred.txt')
+        creds_loc = os.path.join('pgc-code-all', 'config', 'cred.txt')
 
     with open(creds_loc, 'r') as cred:
         content = cred.readlines()
