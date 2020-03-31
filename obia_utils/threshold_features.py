4b64f6e0e8629cfc7174b9a8ba80b2ae25c09ea0
Jeff <jeffrey.disbrow@gmail.com>
2020-03-31 11:54:39 -0500

    working on thresholding...

--- obia_utils/threshold_features.py
+++ obia_utils/threshold_features.py
@@ -27,11 +27,14 @@ module_loggers = create_module_loggers('sh', 'INFO')
 
 
 # Parameters
-slope_thresh = 7.0
-tpi_low_thresh = -0.4 # value threshold for tpi_mean field
+slope_thresh = 5
+tpi_low_thresh = -0.6 # value threshold for tpi_mean field
 tpi_high_thresh = 0.4
-tpis_low = [-0.3, -0.4, -0.5, -0.6, -0.7, -0.8, -0.9, -1]
-tpis_high = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+ndvi_thresh = 0.0
+
+
+# tpis_low = [-0.3, -0.4, -0.5, -0.6, -0.7, -0.8, -0.9, -1]
+# tpis_high = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
 
 # Field names
 # Created
@@ -39,14 +42,20 @@ merge = 'merge'
 steep = 'steep' # features above slope threshold
 neighb = 'neighbors' # field to hold neighbor unique ids
 headwall = 'headwall' # field - bool - headwall = True
+adj_low_tpi_field = 'alt{}'.format(str(tpi_low_thresh).replace('.', 'x').replace('-', 'neg'))
+adj_high_tpi_field = 'aht{}'.format(str(tpi_high_thresh).replace('.', 'x'))
+ndvi_thresh_field = 'ndvi{}'.format(str(ndvi_thresh).replace('.', 'x').replace('-', 'neg'))
 # Existing
 unique_id = 'label'
 slope_mean = 'slope_mean'
 tpi_mean = 'tpi81_mean'
-
+ndvi_mean = 'ndvi_mean'
 
 # Inputs
-seg_path = r'V:\pgc\data\scratch\jeff\ms\2020feb01\aoi6\seg\WV02_20150906_pcatdmx_slope_a6g_sr5_rr1_0_ms400_tx500_ty500_stats.shp'
+# 2015 Slope
+# seg_path = r'V:\pgc\data\scratch\jeff\ms\2020feb01\aoi6\seg\WV02_20150906_pcatdmx_slope_a6g_sr5_rr1_0_ms400_tx500_ty500_stats.shp'
+# 2015 Imagery
+seg_path = r'V:\pgc\data\scratch\jeff\ms\scratch\aoi6_good\seg\WV02_20150906_clip_ms_lsms_sr5rr200ss400_stats.shp'
 tks_bounds_p = r'E:\disbr007\umn\ms\shapefile\tk_loc\digitized_thaw_slumps.shp'
 
 # Load data
@@ -65,36 +74,40 @@ tks = tks.cx[xmin:xmax, ymin:ymax]
 # Determine features above steepness threshold
 seg[steep] = seg[slope_mean] > slope_thresh
 
-# Classify headwall by adjacent to tpi < tpi_low_param and tpi > tpi_high_param
-for t_l in tpis_low:
-    seg = neighbor_adjacent(seg, subset=seg[seg['steep']==True],
-                            unique_id=unique_id,
-                            neighbor_field=neighb,
-                            adjacent_field='alt{}'.format(str(t_l).replace('.', 'x').replace('-', 'neg')),
-                            value_field=tpi_mean,
-                            value_thresh=t_l,
-                            value_compare='<')
+# Steep and adjacent to tpi < tpi_low_param and tpi > tpi_high_param
+seg = neighbor_adjacent(seg, subset=seg[seg['steep']==True],
+                        unique_id=unique_id,
+                        neighbor_field=neighb,
+                        adjacent_field=adj_low_tpi_field,
+                        value_field=tpi_mean,
+                        value_thresh=tpi_low_thresh,
+                        value_compare='<')
 
-for t_h in tpis_high:
-    seg = neighbor_adjacent(seg, subset=seg[seg['steep']==True],
-                            unique_id=unique_id,
-                            neighbor_field=neighb,
-                            adjacent_field='aht{}'.format(str(t_h).replace('.', 'x')),
-                            value_field=tpi_mean,
-                            value_thresh=t_h,
-                            value_compare='>')
+seg = neighbor_adjacent(seg, subset=seg[seg['steep']==True],
+                        unique_id=unique_id,
+                        neighbor_field=neighb,
+                        adjacent_field=adj_high_tpi_field,
+                        value_field=tpi_mean,
+                        value_thresh=tpi_high_thresh,
+                        value_compare='>')
 
+# Steeo and adjacent to NDVI < ndvi_thresh
+seg = neighbor_adjacent(seg, subset=seg[seg['steep']==True],
+                        unique_id=unique_id,
+                        neighbor_field=neighb,
+                        adjacent_field=ndvi_thresh_field,
+                        value_field=ndvi_mean,
+                        value_thresh=ndvi_thresh,
+                        value_compare='<')
 
 # seg[headwall] = np.where(seg['adj_low_tpi'] & seg['adj_high_tpi'], True, False)
-seg.drop(columns=[neighb]).to_file(r'V:\pgc\data\scratch\jeff\ms\2020feb01\aoi6\seg\WV02_20150906_pcatdmx_slope_a6g_sr5_rr1_0_ms400_tx500_ty500_stats_nbs.shp')
+# seg.drop(columns=[neighb]).to_file(r'V:\pgc\data\scratch\jeff\ms\2020feb01\aoi6\seg\WV02_20150906_pcatdmx_slope_a6g_sr5_rr1_0_ms400_tx500_ty500_stats_nbs.shp')
 
 
 ## Get neighbors as shp
 # subset = seg[seg[steep]==True]
 # neighbor_df = neighbor_features(unique_id=unique_id, gdf=seg, subset=subset,
 #                                 neighbor_ids_col=neighb)
-
-# neighbors_df = gp
     
 
 ### Remove class from segmentation
@@ -104,33 +117,37 @@ seg.drop(columns=[neighb]).to_file(r'V:\pgc\data\scratch\jeff\ms\2020feb01\aoi6\
 # out = mask_class(seg, 'h1', img_p, r'/vsimem/test_raster.vrt', mask_value=1)
 
 
-#### Plotting
+### Plotting
 # Set up
-# plt.style.use('ggplot')
-# fig, axes = plt.subplots(2,2, figsize=(10,10))
-# fig.set_facecolor('darkgray')
+plt.style.use('ggplot')
+fig, axes = plt.subplots(2,2, figsize=(10,10))
+fig.set_facecolor('darkgray')
 
-# axes=axes.flatten()
+axes=axes.flatten()
 
 
-# # Plot full segmentation with no fill
-# for ax in axes:
-#     seg.plot(facecolor='none', linewidth=0.5, ax=ax, edgecolor='grey')
-#     # Plot the digitized RTS boundaries
-#     tks.plot(facecolor='none', edgecolor='black', linewidth=2, ax=ax)
-#     ax.set_yticklabels([])
-#     ax.set_xticklabels([])
+# Plot full segmentation with no fill
+for ax in axes:
+    seg.plot(facecolor='none', linewidth=0.5, ax=ax, edgecolor='grey')
+    # Plot the digitized RTS boundaries
+    tks.plot(facecolor='none', edgecolor='black', linewidth=2, ax=ax)
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
 
 # ax = axes[0]
 
-# # Plot the classified features
-# seg[seg[steep]==True].plot(facecolor='b', alpha=0.75, ax=ax)
+# Plot the classified features
+axes[0].set_title(steep)
+seg[seg[steep]==True].plot(facecolor='y', alpha=0.75, ax=axes[0])
 # seg[seg[headwall]==True].plot(facecolor='r', ax=ax, alpha=0.5)
-# # Plot cols
-# axes[1].set_title('adj_low_tpi')
-# seg[seg['adj_low_tpi']==True].plot(facecolor='r', alpha=0.75, ax=axes[1])
-# axes[2].set_title('adj_high_tpi')
-# seg[seg['adj_high_tpi']==True].plot(facecolor='r', alpha=0.75, ax=axes[2])
-# axes[3].set_title('slope')
-# seg[seg[steep]==True].plot(facecolor='r', alpha=0.75, ax=axes[3])
-# plt.tight_layout()
+# Plot cols
+axes[1].set_title(adj_low_tpi_field)
+seg[seg[adj_low_tpi_field]==True].plot(facecolor='b', alpha=0.75, ax=axes[1])
+
+axes[2].set_title(adj_high_tpi_field)
+seg[seg[adj_high_tpi_field]==True].plot(facecolor='r', alpha=0.75, ax=axes[2])
+
+axes[3].set_title(ndvi_thresh_field)
+seg[seg[ndvi_thresh_field]==True].plot(facecolor='g', alpha=0.75, ax=axes[3])
+
+plt.tight_layout()
