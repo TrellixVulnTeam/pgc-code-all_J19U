dcc4444981e1d43f279a3848de294398f1ce7d7c
disbr007 <disbr007@nunatak.pgc.umn.edu>
2020-05-05 15:03:39 -0500

    OS dependent credential location

--- misc_utils/get_creds.py
+++ misc_utils/get_creds.py
@@ -7,16 +7,17 @@ Created on Wed Oct 30 12:49:36 2019
 import os
 import platform
 
-def get_creds(creds_loc=r"C:\code\pgc-code-all\config\cred.txt"):
 
-    ## Credentials for logging into danco
+def get_creds():
+    """Get credentials for logging into danco"""
     creds = []
-    
+
     system = platform.system()
     if system == 'Linux':
         # creds_loc = os.path.join('/mnt', 'pgc', 'data', 'scratch', 'jeff', 'code', 'pgc-code-all', 'config', 'cred.txt')
         creds_loc = os.path.join('pgc-code-all', 'config', 'cred.txt')
-
+    elif system == 'Windows':
+        creds_loc = r"C:\code\pgc-code-all\config\cred.txt"
     with open(creds_loc, 'r') as cred:
         content = cred.readlines()
         for line in content:
