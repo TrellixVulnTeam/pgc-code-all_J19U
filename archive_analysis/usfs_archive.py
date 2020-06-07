a54cd9b69c970af3bed9c6baa80cb04151e0e208
Jeff <jeffrey.disbrow@gmail.com>
2020-06-06 20:19:43 -0500

    added usfs_archive .py

--- archive_analysis/usfs_archive.py
+++ archive_analysis/usfs_archive.py
@@ -4,24 +4,79 @@ Created on Thu Jun  4 20:27:05 2020
 
 @author: disbr007
 """
-
+import os
+import pandas as pd
 import geopandas as gpd
 
 from selection_utils.query_danco import query_footprint, count_table, list_danco_db, layer_fields
+from misc_utils.logging_utils import create_logger
+from misc_utils.id_parse_utils import onhand_ids
 
+logger = create_logger(__name__, 'sh', 'DEBUG')
 
 # Params
-# edem_lyr = 'pgc_earthdem_regions'
-# earthdem = query_footprint(edem_lyr)
+danco_lyr = 'index_dg'
+limit = 25000
+prj_dir = r'V:\pgc\data\scratch\jeff\projects\usfs'
+usfs_p = os.path.join(prj_dir, r'usfs_bounds\usfs_bounds.shp')
+us_state_p = r'E:\disbr007\general\US_States\tl_2017_us_state.shp'
+prj = r'+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs '
 
-regions = ['earthdem_03_conus',
-           'earthdem_04_great_lakes',
-           'arcticdem_34_alaska_north',
-           'arcticdem_31_alaska_south']
+# Load USFS boundaries
+usfs = gpd.read_file(usfs_p)
+usfs = usfs.to_crs(epsg=4326)
+minx, miny, maxx, maxy = usfs.total_bounds
 
+# Query footprint
+where = 'x1 > {} AND x1 < {} AND y1 > {} and y1 < {}'.format(minx, maxx,
+                                                             miny, maxy)
+logger.debug('Where clause for query: {}'.format(where))
 
-# Query footprints
-where = """region_id IN ({})""".format(str(regions)[1:-1])
-count = count_table('dg_imagery_index_stereo', where=where)
+count = count_table(danco_lyr, where=where, table=True)
+logger.debug('Count for table with where clause: {:,}'.format(count))
 
-# fps = query_footprint('index_dg', )
+#%%
+usfs_fps = gpd.GeoDataFrame()
+offset = 0
+while offset < count:
+    logger.debug('Loading records: {:,} - {:,}'.format(offset, offset+limit))
+    # Load footprints
+    fps = query_footprint(danco_lyr, where=where, limit=limit, offset=offset)
+    # Intersect to find USFS footprints
+    logger.debug('Identifying records on USFS land...')
+    slice_usfs_fps = gpd.sjoin(fps, usfs, op='within')
+    logger.debug('USFS records found: {}'.format(len(slice_usfs_fps)))
+    # Merge to master dataframe
+    usfs_fps = pd.concat([usfs_fps, slice_usfs_fps])
+    logger.debug('Total USFS records found: {}'.format(len(usfs_fps)))
+    # Increase offset
+    offset += limit
+
+usfs_fps_catids = list(usfs_fps['catalogid'])
+#%%
+# Remove onhand IDs
+oh = onhand_ids()
+usfs_noh = [x for x in usfs_fps_catids if x not in oh]
+
+#%%
+states = gpd.read_file(us_state_p)
+prj = r'+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs '
+states = states.to_crs(prj)
+usfs = usfs.to_crs(prj)
+usfs_fps = usfs_fps.to_crs(prj)
+#%% Plotting
+import matplotlib.pyplot as plt
+# from mpl_toolkits.basemap import Basemap
+
+plt.style.use('spy4_blank')
+
+fig, ax = plt.subplots(1,1)
+
+states.plot(color='none', edgecolor='white', linewidth=0.5, ax=ax)
+usfs.plot(color='green', edgecolor='green', alpha=0.5, ax=ax)
+usfs_fps.plot(color='red', ax=ax)
+
+minx, miny, maxx, maxy = usfs_fps.total_bounds
+pad = 0
+ax.set_xlim(minx+pad, maxx+pad)
+ax.set_ylim(miny+pad, maxy+pad)
