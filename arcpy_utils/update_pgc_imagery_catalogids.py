82f2508d945c9beebed43aab7131e66946b53608
Jeff <jeffrey.disbrow@gmail.com>
2020-06-12 10:12:32 -0500

    \working update_pgc_imagery_catalogids.py

--- arcpy_utils/update_pgc_imagery_catalogids.py
+++ arcpy_utils/update_pgc_imagery_catalogids.py
@@ -1,3 +1,5 @@
+import argparse
+import datetime
 import logging
 import os
 
@@ -5,26 +7,40 @@ import arcpy
 
 # args
 dryrun = True
-verbose = False
+verbose = True
+logdir = r'C:\temp\log'
+
 
 def get_platform_code(platform):
     platform_code = {
-                'QB02': '101',
-                'WV01': '102',
-                'WV02': '103',
-                'WV03': '104',
-                'WV03-SWIR': '104A',
-                'GE01': '105',
-                'IK01': '106'
-                }
+                    'QB02': '101',
+                    'WV01': '102',
+                    'WV02': '103',
+                    'WV03': '104',
+                    # 'WV03-SWIR': '104A',
+                    'GE01': '105',
+                    'IK01': '106'
+                    }
 
     return platform_code[platform]
+
+
+def cid_from_sid(sid):
+    """Parses a catalog_id from a scene_id"""
+    try:
+        cid = sid.split('_')[2]
+    except IndexError:
+        # TODO: Catch/log this?
+        cid = None
+
+    return cid
 
 
 def get_unique_ids(table, field, where=None, clean_fxn=None):
     """
     Loads unique IDs from the given field in the given table, optionally
-    with the provided where clause
+    with the provided where clause, optionally applying a function to
+    each id before returning.
 
     Parameters:
     table: os.path.abspath
@@ -32,7 +48,9 @@ def get_unique_ids(table, field, where=None, clean_fxn=None):
     field: str
         The field in table to parse.
     where: str
-        SQL where clause to subset table.
+        SQL WHERE clause to subset table.
+    clean_fxn: function
+        Function to apply to each ID before returning.
 
     Returns:
     set: unique values from the given field
@@ -49,16 +67,6 @@ def get_unique_ids(table, field, where=None, clean_fxn=None):
     logger.debug('Unique IDs: {:,}'.format(len(unique_ids)))
 
     return unique_ids
-
-
-def cid_from_sid(sid):
-    """Parses a catalog_id from a scene_id"""
-    try:
-        cid = sid.split('_')[2]
-    except IndexError:
-        cid = None
-
-    return cid
 
 
 def compare_tables(tbl_a, tbl_b,
@@ -90,11 +98,13 @@ def compare_tables(tbl_a, tbl_b,
         The function to apply to each value in table B before comparing.
 
     Returns:
-    tuple: with two sets (missing from table A, missing from table B)
+    tuple: with two sets (missing from table A {b-a}, missing from table B {a-b})
     """
     tbl_a_vals = get_unique_ids(table=tbl_a, field=field_a, where=where_a, clean_fxn=clean_fxn_a)
     tbl_b_vals = get_unique_ids(table=tbl_b, field=field_b, where=where_b, clean_fxn=clean_fxn_b)
+    # Exists in B but not A
     missing_from_a = tbl_b_vals - tbl_a_vals
+    # Exists in A but not B
     missing_from_b = tbl_a_vals - tbl_b_vals
 
     return (missing_from_a, missing_from_b)
@@ -114,54 +124,58 @@ def update_table(sde, table, catid_fld, sensor_fld, new_ids, missing_catids, dry
     sensor_fld: str
         Name of the sensor field to update.
     new_ids: dict
-        [sensor: set of ids to add to table}
+        {sensor: set of ids to add to table}
     missing_catids: set
         ids to remove from table
 
     Returns:
     None
     """
+    # For logging only
+    table_name = os.path.basename(table)
+
     # Start editing
     edit = arcpy.da.Editor(sde)
     edit.startEditing(False, True)
     edit.startOperation()
 
     # Add new IDs
-    logger.info('Appending new catalog IDs to: {}'.format(table))
+    logger.info('Appending new catalog IDs to: {}'.format(table_name))
     with arcpy.da.InsertCursor(table, [catid_fld, sensor_fld]) as icur:
         i = 0
         for platform, cids in new_ids.items():
             for cid in cids:
-                logger.debug('Appending {}: {} - {}'.format(i, cid, platform))
+                # TODO: Do we want to log each ID written or deleted?
+                # logger.debug('Appending {}: {} - {}'.format(i, cid, platform))
                 if not dryrun:
-                    icur.InsertRow([cid, platform])
+                    icur.insertRow([cid, platform])
                 i += 1
     del icur
     edit.stopOperation()
-    logger.info('Records added to {}: {:,}'.format(table, i))
+    logger.info('Records added to {}: {:,}'.format(table_name, i))
 
     if missing_catids:
         # Delete missing
         edit.startOperation()
-        logger.info('Deleting missing catalog IDs from: {}'.format(table))
+        logger.info('Deleting missing catalog IDs from: {}'.format(table_name))
         with arcpy.da.UpdateCursor(table, [catid_fld]) as ucur:
             i = 0
             for row in ucur:
                 catid = row[0]
                 if catid in missing_catids:
-                    logger.debug('Deleting {}'.format(catid))
+                    # logger.debug('Deleting {}'.format(catid))
                     if not dryrun:
                         ucur.deleteRow()
                     i += 1
         del ucur
         edit.stopOperation()
-        logger.info('Records deleted from {}: {}'.format(table, i))
+        logger.info('Records deleted from {}: {}'.format(table_name, i))
 
     edit.stopEditing(True)
 
-    logger.debug('Getting updated count for {}'.format(table))
+    logger.debug('Getting updated count for {}'.format(table_name))
     table_catid_count = int(arcpy.GetCount_management(table).getOutput(0))
-    logger.info('{} updated count: {:,}'.format(table, table_catid_count))
+    logger.info('{} updated count: {:,}'.format(table_name, table_catid_count))
 
 
 #### Logging setup
@@ -173,22 +187,38 @@ else:
 
 logger.setLevel(logging_level)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-# TODO: Add log file
+# Stream handler
+sh = logging.StreamHandler()
+sh.setFormatter(formatter)
+logger.addHandler(sh)
+# File handler
+if not logdir:
+    # TODO: Where should logs go?
+    logdir = os.path.join(os.path.dirname(__file__), 'logs')
+if not os.path.isdir(logdir):
+    os.makedirs(logdir)
+now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+logfile = os.path.join(logdir, '{}_{}.log'.format(os.path.splitext(os.path.basename('update_test'))[0], now))
+fh = logging.FileHandler(logfile)
+fh.setLevel(logging_level)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
 
 #### Paths to sde files and tables
+# dgarchive
+# TODO: Create this sde file in an accessible location
 dgarchive_sde = r'C:\dbconn\sandwich-pool.dgarchive.sde'
 canon_catid_table = 'dgarchive.footprint.canon_catalog_ids'
-canon_catid_table_abs = os.path.join(dgarchive_sde, canon_catid_table)
 canon_sid_tbl = 'dgarchive.footprint.canon_scene_ids'
+canon_catid_table_abs = os.path.join(dgarchive_sde, canon_catid_table)
 canon_sid_tbl_abs = os.path.join(dgarchive_sde, canon_sid_tbl)
-
-danco_sde = r'C:\dbconn\footprint.sde'
+# danco
+# danco_sde = r'C:\dbconn\footprint.sde'
+danco_sde = r'\\files.umn.edu\pgc\trident\db\danco\footprint.sde'
 danco_catid_table = 'footprint.sde.pgc_imagery_catalogids'
-danco_catid_table_abs = os.path.join(danco_sde, danco_catid_table)
 danco_stereo_tbl = 'footprint.sde.pgc_imagery_catalogids_stereo'
+danco_catid_table_abs = os.path.join(danco_sde, danco_catid_table)
 danco_stereo_tbl_abs = os.path.join(danco_sde, danco_stereo_tbl)
 
 #### Constants
@@ -197,7 +227,7 @@ catid_fld = 'catalog_id'
 sid_fld = 'scene_id'
 sensor_fld = 'sensor'
 # TODO: What should sensor be for IDs that don't confirm to platform code convention?
-other = 'other'  # used as key for IDs that don't conform to the platform code
+other = 'other'  # used as 'sensor' for IDs that don't conform to the platform code
 
 
 #### Starting table counts
@@ -206,18 +236,23 @@ for tbl in [danco_catid_table_abs, danco_stereo_tbl_abs]:
     tbl_count = int(arcpy.GetCount_management(tbl).getOutput(0))
     logger.info('{} starting count: {:,}'.format(os.path.basename(tbl), tbl_count))
     starting_counts[tbl] = tbl_count
-
+print('\n')
 
 #### Update danco table: pgc_imagery_catalogids
 logger.info('Determining required updates for {}...'.format(danco_catid_table))
+
+# Create dict of platform name and associated where clause
+platform_wheres = {p: """{} LIKE '{}%'""".format(catid_fld, get_platform_code(p)) for p in platforms}
+# Add catch all where clause for IDs that do not conform to naming convention
+platform_wheres[other] = ' AND '.join(["({} NOT LIKE '{}%')".format(catid_fld,
+                                                                    get_platform_code(p)) for p in platforms])
+
+# Load each platform's IDs for canon_catalog_ids and pgc_imagery_catalogids, find difference
 new_catids = dict()
 missing_catids = set()
-# Load each platform's IDs for canon_catalog_ids and pgc_imagery_catalogids, find difference
-for platform in platforms:
+for platform, where in platform_wheres.items():
     logger.info('Parsing {} IDs...'.format(platform))
-    # Create where clause to select only IDs that start with platform code
-    where = """{} LIKE '{}%'""".format(catid_fld, get_platform_code(platform))
-    # Identify new and missing IDs for platform
+    # Identify new and missing IDs for platform (returns: b-a, a-b)
     new_platform, missing_platform = compare_tables(tbl_a=danco_catid_table_abs,
                                                     tbl_b=canon_catid_table_abs,
                                                     field_a=catid_fld, field_b=catid_fld,
@@ -229,92 +264,42 @@ for platform in platforms:
     missing_catids = missing_catids | missing_platform
 
     del new_platform, missing_platform
-
-# Catch all for any old/other format IDs (that don't start with platform codes)
-logger.info('Parsing all other IDs....')
-where = str()
-for platform in platforms:
-    if where:
-        where += ' AND '
-    where += "({} NOT LIKE '{}%')".format(catid_fld, get_platform_code(platform))
-
-new_other, missing_other = compare_tables(tbl_a=danco_catid_table_abs,
-                                          tbl_b=canon_catid_table_abs,
-                                          field_a=catid_fld, field_b=catid_fld,
-                                          where_a=where, where_b=where)
-
-logger.info('New IDs with unrecognized platform code: {}'.format(len(new_other)))
-logger.info('Missing IDs with unrecognized platform code: {}'.format(len(missing_other)))
-new_catids[other] = new_other
-missing_catids = missing_catids | missing_other
-
-del new_other, missing_other
-logger.info('\n')
+print('\n')
 
 # Perform updates: pgc_catalogids
 logger.info('Making updates to {}'.format(danco_catid_table))
-# if not dryrun:
 update_table(danco_sde, danco_catid_table_abs,
              catid_fld=catid_fld, sensor_fld=sensor_fld,
              new_ids=new_catids, missing_catids=missing_catids,
              dryrun=dryrun)
 
-    # # Start editing
-    # edit = arcpy.da.Editor(danco_sde)
-    # edit.startEditing(False, True)
-    # edit.startOperation()
-    # logger.info('Appending new catalog IDs to: {}'.format(danco_catid_table))
-    # with arcpy.da.InsertCursor(danco_catid_tbl_abs, [catid_fld, sensor_fld]) as icur:
-    #     i = 0
-    #     for platform, cids in new_catids.items():
-    #         for cid in cids:
-    #             logger.debug('Appending {}: {} - {}'.format(i, cid, platform))
-    #             if not dryrun:
-    #                 icur.InsertRow([cid, platform])
-    #                 i += 1
-    # del icur
-    # edit.stopOperation()
-    # logger.info('Records added to {}: {}'.format(danco_catid_table, i))
-    #
-    # # Delete missing
-    # edit.startOperation()
-    # logger.info('Deleting missing catalog IDs from: {}'.format(danco_catid_table))
-    # with arcpy.da.UpdateCursor(danco_catid_table_abs, [catid_fld]) as ucur:
-    #     i = 0
-    #     for row in ucur:
-    #         cid = row[0]
-    #         if catid in missing_catids:
-    #             logger.debug('Deleting {}'.format(catid))
-    #             if not dryrun:
-    #                 ucur.deleteRow()
-    #             i += 1
-    # del ucur
-    # edit.stopOperation()
-    # logger.info('Records deleted from {}: {}'.format(danco_catid_table, i))
-    # logger.debug('Getting updated count for {}'.format(danco_catid_table))
-    # danco_catid_count = int(arcpy.GetCount_management(danco_catid_table_abs).getOutput(0))
-    # logger.info('{} updated count: {}'.format(danco_catid_table, danco_catid_count))
-
 del new_catids, missing_catids
-
-logger.info('\n\n')
+print('\n\n')
 
 
 #### Update danco table: pgc_imagery_ids_stereo
 logger.info('Determining required updates for {}'.format(danco_stereo_tbl))
+# Add P1BS to all canon_scene_id where clauses
+canon_wheres = {p: """{0} LIKE '{1}%' AND {0} LIKE '%P1BS%'""".format(sid_fld, p) for p in platforms}
+# Separate catch all where clause to use platform rather than code
+canon_where_other = ' AND '.join(["({} NOT LIKE '{}%')".format(sid_fld, p) for p in platforms])
+canon_where_other += "  AND ({} LIKE '%P1BS%')".format(sid_fld)
+
 new_stereo_catids = dict()
 missing_stereo_catids = set()
-for platform in platforms:
+for platform, where in platform_wheres.items():
     logger.info('Parsing {} IDs...'.format(platform))
     # Where clauses
-    danco_where = """{} LIKE '{}%'""".format(catid_fld, get_platform_code(platform))
-    canon_where = """{0} LIKE '{1}%' AND {0} LIKE '%P1BS%'""".format(sid_fld, platform)
+    if platform == other:
+        canon_where = canon_where_other
+    else:
+        canon_where = canon_wheres[platform]
     # Identify new and missing IDs for platform
     new_platform, missing_platform = compare_tables(tbl_a=danco_stereo_tbl_abs,
                                                     tbl_b=canon_sid_tbl_abs,
                                                     field_a=catid_fld,
                                                     field_b=sid_fld,
-                                                    where_a=danco_where,
+                                                    where_a=where,
                                                     where_b=canon_where,
                                                     clean_fxn_a=None,
                                                     clean_fxn_b=cid_from_sid)
@@ -325,42 +310,23 @@ for platform in platforms:
     missing_stereo_catids = missing_stereo_catids | missing_platform
 
     del new_platform, missing_platform
-
-# Catch all for any old/other format IDs (that don't start with platform codes)
-logger.info('Parsing all other IDs....')
-danco_where = str()
-canon_where = str()
-for platform in platforms:
-    if danco_where:
-        danco_where += ' AND '
-    danco_where += "({} NOT LIKE '{}%')".format(catid_fld, get_platform_code(platform))
-    if canon_where:
-        canon_where += ' AND '
-    canon_where += "({} NOT LIKE '{}%')".format(sid_fld, platform)
-
-new_other, missing_other = compare_tables(tbl_a=danco_stereo_tbl_abs,
-                                          tbl_b=canon_sid_tbl_abs,
-                                          field_a=catid_fld, field_b=sid_fld,
-                                          where_a=danco_where, where_b=canon_where,
-                                          clean_fxn_a=None, clean_fxn_b=cid_from_sid)
-
-logger.info('New IDs with unrecognized platform code: {:,}'.format(len(new_other)))
-logger.info('Missing IDs with unrecognized platform code: {:,}'.format(len(missing_other)))
-new_stereo_catids[other] = new_other
-missing_stereo_catids = missing_stereo_catids | missing_other
-
-del new_other, missing_other
-logger.info('\n')
+print('\n')
 
 # Perform updates: pgc_imagery_catalogids_stereo
 update_table(danco_sde, danco_stereo_tbl_abs,
              catid_fld=catid_fld, sensor_fld=sensor_fld,
              new_ids=new_stereo_catids, missing_catids=missing_stereo_catids,
              dryrun=dryrun)
-
+del new_stereo_catids, missing_stereo_catids
+print('\n\n')
 
 # Compare starting counts and ending counts
 for tbl in [danco_catid_table_abs, danco_stereo_tbl_abs]:
-    logger.info('{} starting count: {:,}'.format(os.path.basename(tbl), starting_counts[tbl]))
-    tbl_count = int(arcpy.GetCount_management(tbl).getOutput(0))
-    logger.info('{} ending count:   {:,}'.format(os.path.basename(tbl), tbl_count))
+    tbl_name = os.path.basename(tbl)
+    logger.info('{} starting count: {:,}'.format(tbl_name, starting_counts[tbl]))
+    ending_count = int(arcpy.GetCount_management(tbl).getOutput(0))
+    logger.info('{} ending count:   {:,}'.format(tbl_name, ending_count))
+    logger.info('{} change:         {:,}'.format(tbl_name, ending_count-starting_counts[tbl]))
+    print('\n')
+
+logger.info('Done.')
