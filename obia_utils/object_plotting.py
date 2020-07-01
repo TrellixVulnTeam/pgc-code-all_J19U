aa4bafd366ea14ce8bc3f5e6f0050815d2bbc7dd
disbr007 <disbr007@umn.edu>
2020-07-01 17:52:57 -0500

    plotting to extent of objects fixed

--- obia_utils/object_plotting.py
+++ obia_utils/object_plotting.py
@@ -14,31 +14,26 @@ from misc_utils.logging_utils import create_logger
 
 logger = create_logger(__name__, 'sh', 'DEBUG')
 
+#%%
 
-plt.style.use('pycharm')
 
-obj_p = r'V:\pgc\data\scratch\jeff\ms\2020may12\seg\WV02_20140818_pca_MDFM_sr5_rr0x5_ms50_tx500_ty500.shp'
-img_p = r'V:\pgc\data\scratch\jeff\ms\2020may12\imagery\ps_clip\WV02_20140818201020_1030010035755C00_14AUG18201020-M1BS_R01C1-500106196120_03_P001_u16rf3413_clip.tif'
+def geo2pixel(y, x, img):
+    """
+    Convert geographic coordinates to pixel coordinates
+    """
+    xres, xrot, ulx, yrot, yres, uly, *_rest = img.transform
+    geotransform = [ulx, xres, xrot, uly, yrot, yres]
+    row = int(np.around((y - geotransform[3]) / geotransform[5]))
+    col = int(np.around((x - geotransform[0]) / geotransform[1]))
 
-# Open data sources
-logger.debug('Opening objects...')
-obj = gpd.read_file(obj_p)
-logger.debug('Opening imagery...')
-img = rio.open(img_p)
-logger.debug('Loading array')
-img_arr = img.read()
-logger.debug('Getting plotting extent..')
-img_ext = plotting_extent(img)
-logger.debug('Converting object crs to image crs..')
-obj = obj.to_crs(img.crs)
+    return row, col
 
-img_kwargs={'stretch': True}
 
-def plot_objects(obj=None, img=None, column=None, bounds_only=True,
-                 cmap='viridis', linewidth=0.5, alpha=1,
+def plot_objects(obj=None, img=None, column=None, bounds_only=True, obj_extent=True,
+                 obj_cmap=None, linewidth=0.5, alpha=1,
                  edgecolor='white', rgb=[4, 2, 1], band=None,
                  ax=None, obj_kwargs={}, img_kwargs={}):
-    """Plot vector objects on an image.
+    """Plot vector objects on an image
     Parameters:
         """
     # Create a figure and ax is not provided
@@ -47,21 +42,30 @@ def plot_objects(obj=None, img=None, column=None, bounds_only=True,
     # Plot the img if provided
     if img is not None:
         logger.debug('Plotting imagery...')
-        if isinstance(img, rio.io.DatasetReader):
-            img_arr = img.read()
+        # If path to image provided, open it, otherwise assumed open rasterio DatasetReader
+        if isinstance(img, str):
+            img = rio.open(img)
+        img_arr = img.read(masked=True)
+        if obj is not None and obj_extent:
+            minx, miny, maxx, maxy = obj.total_bounds
+            minrow, mincol = geo2pixel(y=maxy, x=minx, img=img)
+            maxrow, maxcol = geo2pixel(y=miny, x=maxx, img=img)
+            img_arr = img_arr[:, mincol:maxcol, minrow:maxrow]
+            logger.debug(img_arr.min(), img_arr.max())
+            img_ext = (minx, maxx, miny, maxy)
+        else:
             img_ext = plotting_extent(img)
-        elif isinstance(img, os.path.abspath):
-            i = rio.open(img)
-            img_arr = i.read()
-            img_ext = ep.plotting_ext(i)
-        if band:
+        logger.debug('Img ext: {}'.format(img_ext))
+        if band is not None:
             ep.plot_bands(img_arr[band], extent=img_ext, ax=ax, **img_kwargs)
         else:
             ep.plot_rgb(img_arr, rgb=rgb, ax=ax, extent=img_ext, **img_kwargs)
+
     # Plot the objects if provided
     if obj is not None:
         logger.debug('Plotting objects...')
-        obj = obj.to_crs(img.crs)
+        if img is not None:
+            obj = obj.to_crs(img.crs)
         # If a column is not provided, plot all as the same color
         if column is None:
             # Create temporary column, ensuring it doesn't exist
@@ -72,12 +76,28 @@ def plot_objects(obj=None, img=None, column=None, bounds_only=True,
 
             obj[column] = 1
         if bounds_only:
-            obj.set_geometry(obj.geometry.boundary).plot(ax=ax, column=column, cmap=cmap, alpha=alpha,
+            obj.set_geometry(obj.geometry.boundary).plot(ax=ax, column=column, cmap=obj_cmap, alpha=alpha,
                                                          linewidth=linewidth, **obj_kwargs)
         else:
-            obj.plot(ax=ax, column=column, cmap=cmap, alpha=alpha,
+            obj.plot(ax=ax, column=column, cmap=obj_cmap, alpha=alpha,
                      linewidth=linewidth, edgecolor=edgecolor, **obj_kwargs)
+
 
     fig.show()
 
-plot_objects(obj=obj, img=img, rgb=[5,3,2], bounds_only=True, img_kwargs=img_kwargs)
+
+#%%
+# plt.style.use('ggplot')
+#
+# obj_p = r'V:\pgc\data\scratch\jeff\ms\2020may12\seg\grm\WV02_20140818_pca_MDFM_bst10x0ni30s0spec1x0spat0x5.shp'
+# img_p = r'V:\pgc\data\scratch\jeff\ms\2020may12\dems\pca\max_diff_from_mean\WV02_20140818_pca_MDFM.tif'
+# # %%
+# # Open data sources
+# logger.debug('Opening objects...')
+# seg = gpd.read_file(obj_p)
+# logger.debug('Opening imagery...')
+# img = rio.open(img_p)
+#
+# img_kwargs = {}
+# plot_objects(obj=seg, img=img, band=0, bounds_only=True, obj_extent=True, obj_cmap=None,
+#              img_kwargs=img_kwargs, obj_kwargs={'color': 'white'})
--- rts/classify_rts.py
+++ rts/classify_rts.py
@@ -5,41 +5,66 @@ Created on Thu May 14 12:19:21 2020
 @author: disbr007
 """
 import copy
-# import operator
+import operator
+import matplotlib.pyplot as plt
 
 import pandas as pd
 # from pandas.api.types import is_numeric_dtype
 import geopandas as gpd
+from shapely.geometry import Polygon
 
 from misc_utils.logging_utils import create_logger
 from obia_utils.neighbors import subset_df, adj_neighbor
+from obia_utils.object_plotting import plot_objects
 
 pd.options.mode.chained_assignment = None
 
 logger = create_logger(__name__, 'sh', 'DEBUG')
 
+plt.style.use('pycharm')
 #%%
-from datetime import datetime
-start = datetime.now()
-
-logger.debug('Reading in segmentation...')
+# Paths
+tks_path = r'V:\pgc\data\scratch\jeff\ms\2020may12\tks_loc\lewk_2019_hs_selected_3413.shp'
 # seg_path = r'V:\pgc\data\scratch\jeff\ms\scratch\aoi6_good\seg\WV02_20150906_tpi31_tpi81_tpi101_stk_a6g_sr5_rr0x35_ms100_tx500_ty500_stats_wbt.shp'
-seg_path = r'V:\pgc\data\scratch\jeff\ms\2020feb01\aoi6\seg\WV02_20150906_pcatdmx_slope_a6g_sr5_rr1_0_ms400_tx500_ty500_stats_nbs_wbt.shp'
-seg = gpd.read_file(seg_path)
+# seg_path = r'V:\pgc\data\scratch\jeff\ms\2020feb01\aoi6\seg\WV02_20150906_pcatdmx_slope_a6g_sr5_rr1_0_ms400_tx500_ty500_stats_nbs_wbt.shp'
+seg_path = r'V:\pgc\data\scratch\jeff\ms\2020may12\seg\grm\WV02_20140818_pca_MDFM_bst10x0ni30s0spec1x0spat0x5.shp'
+img_p = r'V:\pgc\data\scratch\jeff\ms\2020may12\dems\pca\max_diff_from_mean\WV02_20140818_pca_MDFM.tif'
+
 # Existing columns
 unique_id = 'label'
-# tpi31_mean = 'tpi31_mean'
-# slope_mean = 'slope_mean'
+tpi31_mean = 'tpi31_mean'
+slope_mean = 'slope_mean'
+
+# %% Load segmentation
+logger.debug('Reading in segmentation...')
+seg = gpd.read_file(seg_path)
+
+# %% Reduce to just area around selected tks points
+tks = gpd.read_file(tks_path)
+if seg.crs != tks.crs:
+    tks = tks.to_crs(seg.crs)
+tks.geometry = tks.buffer(200)
+
+bminx, bminy, bmaxx, bmaxy = tks.total_bounds
+border_points = [(bminx, bminy), (bminx, bmaxy), (bmaxx, bmaxy), (bmaxx, bminy)]
+
+border = gpd.GeoDataFrame(geometry=[Polygon(border_points)])
+seg = gpd.overlay(seg, border)
+seg = seg.drop_duplicates(subset='label')
+
+# %% Plot objects on image
+plot_objects(obj=seg, img=img_p, band=0, obj_extent=True)
+
+# %%
 # Remove extra columns for testing
-# seg = seg[[unique_id, tpi31_mean, slope_mean, 'geometry']]
+seg = seg[[unique_id, tpi31_mean, slope_mean, 'geometry']]
 
 # Set index
 seg.set_index(unique_id, inplace=True)
 logger.debug('DataFrame has unique index: {}'.format(str(seg.index.is_unique)))
 
-#%% Determine headwalls
-# ID Steep
-seg['steep'] = seg['slope_mean'] > 10
+# %%
+
 
 #%% Params
 
@@ -47,32 +72,8 @@ hw_params = [('slope_mean',  '>', 6.5)]
 
 hw = subset_df(seg, params=hw_params)
 
-# Plot
-import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-# from matplotlib.transforms import Bbox
-
 vc = 'MDEC_media'
 plt.style.use('spy4_blank')
 fig, ax = plt.subplots(1,1)
 
-# ax.axis('off')
-# ax.tick_params(axis='both', reset=True)
-divider = make_axes_locatable(ax)
-cax = divider.append_axes("right", size="3%", pad=0.2)
-
-# seg.plot(ax=ax, column=vc, edgecolor='white', linewidth=0.75, linestyle=':', 
-        # legend=True, cax=cax,
-        # vmin=0, vmax=0.3)
-hw.plot(ax=ax, column='steep', edgecolor='black', linewidth=0.25)
-
-# ax.axis([-1752000, -1751600, -559300, -558910])
-for spine in ['bottom', 'top', 'left', 'right']:
-    ax.spines[spine].set_color('white')
-    ax.spines[spine].set_linewidth(1)
-    ax.spines[spine].set_visible(True)
-
 plt.tight_layout(pad=3)
-# plt.savefig(r'C:\code\jeff-diz.github.io\images\merge_closest_neighbor\slope_thr_base_mcn_base.png'.format(vt,min_size),
-#             facecolor='#19232d',
-#             dpi=300)
