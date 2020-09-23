cc5b99fe202164fc82f738b7c2b3c62b643d5307
disbr007 <disbr007@umn.edu>
2020-09-23 13:59:45 -0500

    updates

--- tests/OpenFileGDB_support.py
+++ tests/OpenFileGDB_support.py
@@ -1,3 +1,6 @@
+from osgeo import gdal
+
+
 def open_file_gdb_support():
 	try:
 	    driver = gdal.GetDriverByName('OpenFileGDB')
@@ -5,3 +8,5 @@ def open_file_gdb_support():
 	        print('OpenFileGDB driver found.')
 	    else:
 	        print('OpenFileGDB driver not found.')
+
+open_file_gdb_support()
