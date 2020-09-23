7bd27ef23433614d5a0092500eb9cdd322bb99e4
disbr007 <disbr007@umn.edu>
2020-09-23 14:22:17 -0500

    generalized gdal_driver_tests.py

--- tests/gdal_driver_tests.py
+++ tests/gdal_driver_tests.py
@@ -11,6 +11,22 @@ def driver_support(driver_name):
 		print('Error locating {} driver.'.format(driver_name))
 		print(e)
 
+def test_bigtiff():
+	md = gdal.GetDriverByName('GTiff').GetMetadata()
+	try:
+		test = md['DMD_CREATIONOPTIONLIST'].find('BigTIFF')
+		if test != -1:
+			print('Success: {}'.format(test))
+		else:
+			print('Failure: {}'.format(test))
+	except Exception as e:
+		print('BigTIFF Unsupported')
+		print(e)
+
+test_bigtiff()
+
 drivers = ['OpenFileGDB', 'FileGDB', 'ESRI FileGDB']
 for d in drivers:
     driver_support(d)
+
+test_bigtiff()
