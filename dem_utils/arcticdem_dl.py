392b0871551f8422e2c3e9b206d1a327a271ba41
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-28 03:38:15 +0000

    Adding tarfile member sanitization to extractall()

--- dem_utils/arcticdem_dl.py
+++ dem_utils/arcticdem_dl.py
@@ -86,7 +86,26 @@ def dl_arcticdem_strips(fp_p, dst_dir, url_fields=['fileurl'], log_file=None,
         if not os.path.exists(dst_dem):
             logger.debug('Unzipping: {}'.format(gz))
             with tarfile.open(gz, 'r:gz') as tar:
-                tar.extractall(dem_subdir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, dem_subdir)
     
     # Add local filepath fields to footprint 
     if out_filepath_fld:
